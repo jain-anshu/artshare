2b5d5eb2700ea23ade07c0320f8a31d4950b3d1a
jain-anshu <anshuj@gmail.com>
2023-10-09 13:48:11 -0700

    Adds routes for users and artworks resources

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+    def index
+      @users = User.all
+      render json: @users, status: :ok
+    end
+    
+    def show
+      @user = User.find(params[:id])
+      render json: @user, status: :ok
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save 
+        render json: @user, status: :created 
+      else 
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def destroy 
+      @user = User.find(params[:id])
+      
+      @user.destroy 
+
+      render json: @user 
+    end
+    
+    def update 
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        render json: @user, status: :ok
+      else
+        render json: @user.errors.full_messages
+      end
+    end        
+    
+    private
+    def user_params
+        params.require(:user).permit(:username)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ArtworkShare.destroy_all
+Artwork.destroy_all
+User.destroy_all
+user = User.create([
+    {username: "User1"},
+    {username: "User2"},
+    {username: "User3"},
+    {username: "User4"},
+    {username: "User5"},
+    {username: "User6"},
+    {username: "User7"}
+])
+artworks = Artwork.create([
+    {title: "starry nights", image_url: "www.artsare.com/art1", artist_id: User.find_by(username: "User1").id},
+    {title: "ballerina", image_url: "www.artsare.com/art2", artist_id: User.find_by(username: "User1").id},
+    {title: "sunflowers", image_url: "www.artsare.com/art3", artist_id: User.find_by(username: "User2").id},
+    {title: "two sisters", image_url: "www.artsare.com/art4", artist_id: User.find_by(username: "User1").id},
+    {title: "monalisa", image_url: "www.artsare.com/art5", artist_id: User.find_by(username: "User2").id}
+])
+
+artwork_shares = ArtworkShare.create([
+    {artwork_id: Artwork.find_by(title: "starry nights").id, viewer_id: User.find_by(username: "User2").id},
+    {artwork_id: Artwork.find_by(title: "starry nights").id, viewer_id: User.find_by(username: "User3").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User1").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User3").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User2").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User4").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User6").id},
+    {artwork_id: Artwork.find_by(title: "sunflowers").id, viewer_id: User.find_by(username: "User4").id},
+    {artwork_id: Artwork.find_by(title: "sunflowers").id, viewer_id: User.find_by(username: "User5").id},
+    {artwork_id: Artwork.find_by(title: "sunflowers").id, viewer_id: User.find_by(username: "User6").id},
+    {artwork_id: Artwork.find_by(title: "monalisa").id, viewer_id: User.find_by(username: "User7").id}
+])
